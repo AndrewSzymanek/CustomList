95a1366c1f961c19076359fe76fef30350701ec2
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2019-11-05 14:21:43 -0600

    added a couple unit tests

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
 
 namespace UnitTestProject1
 {
@@ -7,8 +8,39 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CustomList_Add_AddingOneValueCount()
         {
+            //arrange
+            CustomList<int> list1 = new CustomList<int>();
+            int expected = 1;
+            int actual;
+            int number1 = 5;
+            
+
+            //act
+            list1.Add(number1);
+            actual = list1.Count;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+
         }
+        [TestMethod]
+        public void CustomList_Add_ResizingCapacityOfArray()
+        {
+            if(list1.Capacity >= 4)
+            {
+                new CustomList<T>();
+                
+            }
+
+        }
+        [TestMethod]
+        public void
+
+
+        [TestMethod]
+        public void
     }
 }
